2b29229a5fb05650f2d0b5ca11872c1a7645d5a6
Andrey Salnikov <kvanta13@yandex.ru>
2022-04-18 00:45:56 +0300

    Add project files.

---
+++ RevitAPICopyGroup/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICopyGroup
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            Reference reference = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Selected group elements");
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+
+            XYZ point = uidoc.Selection.PickPoint("Select point");
+
+            Transaction ts = new Transaction(doc);
+            ts.Start();
+            doc.Create.PlaceGroup(point, group.GroupType);
+            ts.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
